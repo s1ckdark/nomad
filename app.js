3a36fbfe87562788750aa25e98b269d03aae69ec
dave <dave.lee@uniquedonut.com>
2017-01-31 15:14:22 +0900

    build pages

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/contact', routes);
 app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Nomad Seoul' });
 });
 
+router.get('/contact', function(req, res){
+    res.render('contact', { title: 'Contact' });
+})
 module.exports = router;
